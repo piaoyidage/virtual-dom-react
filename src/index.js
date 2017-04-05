46d4602e41022263dec29d5109da545d15fc05c1
piaoyidage <longyixiaoge@gmail.com>
2017-04-05 11:57:54 +0800

    更新节点

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const a = (
 );
 
 console.log(a);
+console.log(typeof a);
 
 /**
  * [createElement 根据 virtual dom 创建真实 dom]
@@ -34,3 +35,53 @@ function createElement(node) {
 	   .forEach($el.appendChild.bind($el));
 	return $el;
 }
+
+/**
+ * [updateElement 更新dom节点]
+ * @param  {[type]} $parent [virtual node 的父亲节点]
+ * @param  {[type]} newNode [新的 virtual node]
+ * @param  {[type]} oldNode [旧的 virtual node]
+ * @param  {Number} index   [新的 virtual node 在父亲节点的位置]
+ * @return {[type]}         [description]
+ */
+function updateElement($parent, newNode, oldNode, index = 0) {
+	 if (!oldNode) {
+	 	// 老节点 没有，增加新节点
+	 	$parent.appendChild(createElement(newNode));
+	 } else if (!newNode) {
+	 	// 新节点 没有，删除老节点
+	 	$parent.removeChild($parent.childNodes[index]);
+	 } else if (changed(newNode, oldNode)){
+	 	// 新节点和老节点 发生变化，替换
+	 	$parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+	 } else {
+	 	// 遍历子节点 再比较更新
+	 	let newNodeLength = newNode.children.length,
+	 		oldNodeLength = oldNode.children.length;
+
+	 	for (let i = 0; i < newNodeLength || i < oldNodeLength; i++){
+	 		updateElement($parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+	 	}
+	 }
+}
+
+/**
+ * [changed 判断两个节点是否发生了改变]
+ * @param  {[type]} node1 [description]
+ * @param  {[type]} node2 [description]
+ * @return {[type]}       [description]
+ */
+function changed(node1, node2) {
+	return typeof node1 !== typeof node2 ||
+		   typeof node1 === 'string' && node1 !== node2 ||
+		   typeof node1.type !== node2.type;
+}
+
+
+
+
+
+
+
+
+
