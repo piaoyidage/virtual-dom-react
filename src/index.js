d9e3bacb779bcc038f7d279bbab457fd487641b1
piaoyidage <longyixiaoge@gmail.com>
2017-04-05 10:50:03 +0800

    自定义 jsx 语法

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+/**
+ * 自定义 jsx
+ */
+
+/** @jsx h */
+function h(type, props, ...children) {
+  return { type, props, children };
+}
+
+const a = (
+  <ul className="list">
+    <li>item 1</li>
+    <li>item 2</li>
+  </ul>
 );
+
+console.log(a);
